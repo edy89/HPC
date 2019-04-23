ad32b1b0bfa1bd15a9e2eb5f82d6406917faa586
kalugy <danieltrujillo901@gmail.com>
2019-04-23 16:34:07 -0500

    arreglohilos

--- HPC-ejericiomatrices/Versiondan/matrices_parallel.c
+++ HPC-ejericiomatrices/Versiondan/matrices_parallel.c
@@ -5,133 +5,234 @@
 # include <unistd.h>
 # include <pthread.h>
 # include <string.h>
-void poblar(int **,int ,int );
-void mostrar(int **,int ,int );
-void multiplicar(void **,void **,void ** );
-unsigned long __stdcall mithread( void* pVoid);
-typedef struct _Params
+
+
+typedef struct 
 {
-  int **mat;
-  int **mat2;
-  int **resultado;
-  int dimension;
-}Params, *PParams;
- 
-unsigned long __stdcall mithread( void* pVoid)
+  int **matriz,dimension;
+  char id;
+}pack;
+
+
+typedef struct 
 {
-  PParams *m = (PParams)pVoid;
-  int temporal,i,j,k;
-  for (i = 0 ; i < m -> dimension ; i++ ) //i para las filas de la matriz resultante
-   {
-    for (k = 0 ; k < m -> dimension ; k++ ) // k para las columnas de la matriz resultante
-       {  
-        temporal = 0 ;
-        for (j = 0 ; j < m -> dimension ; j++ ) //j para realizar la multiplicacion de 
-            { 
-             temporal += m -> mat[i][j] * m -> mat2[j][k];               
-             m -> resultado[i][k] = temporal;
-            }
-        }
-  }
+  int **matrizA,**matrizB,**matriz,dimension;
+  char id;
+}pack_matrices;
+
+
+void *imprimir(void *args)
+{
+  pack  *m = (pack *)args;
+  int i,j;
+  printf("\nMatriz %c: ", m -> id);
+    for(i=0;i < m -> dimension ;i++)
+       {
+        printf("\n\t\t");
+        for(j = 0;j < m -> dimension ; j++)
+           {
+          printf("[ %d ]", m -> matriz[i][j]);
+         }
+    }
+  printf("\n");
+}
+
+/*void *llenar_matriz(void *args)
+{
+  pack  *m = (pack *)args;
+  int i,j,x;
+    for(i=0;i < m -> dimension ;i++)
+       {
+        for(j = 0;j < m -> dimension ; j++)
+           {
+          m -> matriz[i][j] = rand() % 11;
+         }
+    }
+}*/
+
+
+void *llenar_matriz(void *args)
+{
+  pack_matrices  *m = (pack_matrices *)args;
+  int i,j,x;
+
+    for(i=0;i < m -> dimension ;i++)
+       {
+        for(j = 0;j < m -> dimension ; j++)
+           {
+          m -> matrizA[i][j] = rand() % 11;
+         }
+    }
+
+  for(i=0;i < m -> dimension ;i++)
+       {
+        for(j = 0;j < m -> dimension ; j++)
+           {
+          m -> matrizB[i][j] = rand() % 11;
+         }
+    }
+
+    for(i=0;i < m -> dimension ;i++)
+       {
+        printf("\n\t\t");
+        for(j = 0;j < m -> dimension ; j++)
+           {
+          printf("[ %d ]", m -> matrizA[i][j]);
+         }
+    }
+    for(i=0;i < m -> dimension ;i++)
+       {
+        printf("\n\t\t");
+        for(j = 0;j < m -> dimension ; j++)
+           {
+          printf("[ %d ]", m -> matrizB[i][j]);
+         }
+    }
 
 
 }
- 
+
+
+void *multiplicar_matrices(void *args)
+{
+  pack_matrices  *m = (pack_matrices *)args;
+  int i,j,k,temporal;
+
+    for (i = 0 ; i < m -> dimension ; i++ ) //i para las filas de la matriz resultante
+       {
+        for (k = 0 ; k < m -> dimension ; k++ ) // k para las columnas de la matriz resultante
+           {  
+            temporal = 0 ;
+            for (j = 0 ; j < m -> dimension ; j++ ) //j para realizar la multiplicacion de 
+                { 
+                 temporal += m -> matrizA[i][j] * m -> matrizB[j][k];               
+                 m -> matriz[i][k] = temporal;
+                }
+            }
+      }
+    /*  
+    printf("%s", "MAtriz Resultado: \n");
+    for(i=0;i < m -> dimension ;i++)
+       {
+        printf("\n\t\t");
+        for(j = 0;j < m -> dimension ; j++)
+           {
+          printf("[ %d ]", m -> matriz[i][j]);
+         }
+    }*/
+}
+void *multiplicar_matricesfila(void *args)
+{
+  pack_matrices  *m = (pack_matrices *)args;
+  int i,j,k,temporal;
+
+    for (i = 0 ; i < m -> dimension ; i++ ) //i para las filas de la matriz resultante
+       {
+        for (k = 0 ; k < m -> dimension ; k++ ) // k para las columnas de la matriz resultante
+           {  
+            temporal = 0 ;
+            for (j = 0 ; j < m -> dimension ; j++ ) //j para realizar la multiplicacion de 
+                { 
+                 temporal += m -> matrizA[i][j] * m -> matrizB[j][k];               
+                 m -> matriz[i][k] = temporal;
+                }
+            }
+      }
+    /*  
+    printf("%s", "MAtriz Resultado: \n");
+    for(i=0;i < m -> dimension ;i++)
+       {
+        printf("\n\t\t");
+        for(j = 0;j < m -> dimension ; j++)
+           {
+          printf("[ %d ]", m -> matriz[i][j]);
+         }
+    }*/
+}
 
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
 
-    int filas,columnas;
-    scanf("%d%d",&filas,&columnas);
-    int **matriz;
-    int x=3;
+
+    int i,j;
+    srand (time(NULL));
+    char *c;
+    long op=strtol(argv[1],&c,10) ;
+    //printf("Hola %lu",op);
+    clock_t start_t, end_t;
+    double duration;
+
+
+    
+    int parametro,x;
+    parametro=atoi(argv[1]);
+    x=parametro;
+
+   
     pthread_t hilo1,hilo2,hilo3,hilo4,hilo5,hilo6;
 
+    pack packmA,packmB;
+    pack_matrices mul_matrices;
+    packmA.dimension = x;
+    packmB.dimension = x;
+    mul_matrices.dimension = x;
+    packmA.id = 'A';
+    packmB.id = 'B';
+    mul_matrices.id = 'R';
     
-    matriz= (int**) calloc(filas,sizeof(int *));
-    if (matriz == NULL) return 0;
-    int i;
-    for(i=0;i<filas;i++){
-        matriz[i]= (int*) calloc(columnas,sizeof(int ));
-        if(matriz[i] == NULL) return 0;
-    }
 
-    //Reservamos memoria para las matrices con calloc las llenamos de 0
-    int **mat,**mat2, **resultado;
-    mat = (int**) calloc(x,sizeof(int *));
-    mat2= (int**) calloc(x,sizeof(int *));
-    resultado = (int**) calloc(x,sizeof(int *));
+    //mat = (int **)calloc(x,sizeof(int *));
+    packmA.matriz = (int **)calloc(x,sizeof(int *));
+    packmB.matriz = (int **)calloc(x,sizeof(int *));
+    mul_matrices.matriz = (int **)calloc(x,sizeof(int *));
+    mul_matrices.matrizA = (int **)calloc(x,sizeof(int *));
+    mul_matrices.matrizB = (int **)calloc(x,sizeof(int *));
 
+    for(i=0;i<x;i++)
+       {
+        packmA.matriz[i] = (int *)calloc(x,sizeof(int));
+        packmB.matriz[i] = (int *)calloc(x,sizeof(int));
+        mul_matrices.matriz[i] = (int *)calloc(x,sizeof(int));
+        mul_matrices.matrizA[i] = (int *)calloc(x,sizeof(int));
+        mul_matrices.matrizB[i] = (int *)calloc(x,sizeof(int));
+       }
+         
+  pthread_create(&hilo1,NULL,llenar_matriz,(void *)&mul_matrices);
+  //pthread_create(&hilo2,NULL,llenar_matriz,(void *)&packmB);
+  pthread_join(hilo1,NULL);
+  //mul_matrices.matrizA = packmA.matriz;
+  //pthread_join(hilo2,NULL);
+  //mul_matrices.matrizB = packmB.matriz;
+  //pthread_create(&hilo4,NULL,imprimir,(void *)&packmA);
+  //pthread_join(hilo4,NULL);
+  //pthread_create(&hilo5,NULL,imprimir,(void *)&packmB);
+  //pthread_join(hilo5,NULL);
+  start_t = clock();
 
-    if (mat == NULL) return 0;
-    if (mat2 == NULL) return 0;
-    if (resultado == NULL) return 0;
-    for(i=0;i<x;i++){
-        mat[i]= (int *)calloc(x,sizeof(int ));
-        mat2[i]= (int *)calloc(x,sizeof(int ));
-        resultado[i]= (int *)calloc(x,sizeof(int ));
-        if(mat[i] == NULL) return 0;
-        if(mat2[i] == NULL) return 0;
-        if(resultado[i] == NULL) return 0;
-    }
-
-
-    poblar(matriz,filas,columnas);
-    //mostrar(matriz,filas,columnas);
-    //multiplicar(matriz,matriz,resultado,filas);
-    Params params;
-    params.mat = matriz;
-    params.mat2 = matriz;
-    params.dimension = 3;
-    CreateThread( 0, 0, &mithread, &params, 0, 0 );
-
-    //pthread_create(&hilo1,NULL,multiplicar,((void *)matriz,(void *)matriz,(void *)resultado));
-    //pthread_join(hilo1,NULL);
+  pthread_create(&hilo3,NULL,multiplicar_matrices,(void *)&mul_matrices);
+  pthread_join(hilo3,NULL);
+  end_t = clock();
+  duration = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+  printf("%f\n",duration);
 
 
+  for (i = 0;i < x;i++)
+   {
+    free(packmB.matriz[i]);
+    free(packmA.matriz[i]);
+    free(mul_matrices.matriz[i]);
+    free(mul_matrices.matrizA[i]);
+    free(mul_matrices.matrizB[i]);
+   }
+ 
+  free(packmA.matriz);
+  free(packmB.matriz);
+  free(mul_matrices.matriz);
+  free(mul_matrices.matrizA);
+  free(mul_matrices.matrizB);
 
-    mostrar(resultado,filas,columnas);
-    return 0;
-}
-void poblar (int **matriz,int filas,int columnas){
-    srand(time(NULL));
-    int i,j;
-    for(i=0;i<filas;i++){
-        for(j=0;j<columnas;j++){
-            matriz[i][j]= rand()%10;
-        }
-    }
-}
-
-
-void mostrar (int **matriz,int filas,int columnas){
-    int i,j;
-    for(i=0;i<filas;i++){
-        for(j=0;j<columnas;j++){
-            printf("%d ",matriz[i][j]);
-        }
-        printf("\n");
-    }
-    
+ return 0;
 }
 
-
-void multiplicar (void **mat,void **mat2,void **resultado){
-
-  int temporal,i,j,x,k;
-  x=3;
-  for (i = 0 ; i < x ; i++ ) //i para las filas de la matriz resultante
-     {
-      for (k = 0 ; k < x ; k++ ) // k para las columnas de la matriz resultante
-         {  
-          temporal = 0 ;
-          for (j = 0 ; j < x ; j++ ) //j para realizar la multiplicacion de 
-              {                                   //los elementos   de la matriz
-               temporal += mat[i][j] * mat2[j][k];
-               resultado[i][k] = temporal ;
-              }
-          }
-    }
-}
