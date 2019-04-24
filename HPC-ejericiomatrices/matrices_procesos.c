dca5057cee4962100c679c929f5a1901412573ac
edy89 <edy89@users.noreply.github.com>
2019-04-23 22:19:57 -0500

    Update matrices_procesos.c

--- HPC-ejericiomatrices/matrices_procesos.c
+++ HPC-ejericiomatrices/matrices_procesos.c
@@ -1,152 +1,145 @@
-#include <stdio.h>
+//cd C:\Users\Edyson\GitHub\HPC\HPC-ejericiomatrices
+#include <sys/types.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <time.h>
-//#include <string.h>
-//https://computing.llnl.gov/tutorials/parallel_comp/
-//https://www.um.es/earlyadopters/actividades/a3/PCD_Activity3_Session1.pdf
-int main(int argc, char *argv[])
+
+/*void reservar_matrices(int **x,int **y, int **z, int c);
+void liberar_matrices(int **x,int **y, int **z, int c);
+void llenar_matriz(int **x,int c);
+int **multiplicar(int **x,int **y,int c);
+void imprimir_matriz(int **x,int c);*/
+ 
+void reservar_matrices(int **x,int **y, int **z, int c)
 {
-	
-   int count;
+  int i;
+  x = (int **)calloc(c,sizeof(int *));
+  y= (int **)calloc(c,sizeof(int *));
+  z = (int **)calloc(c,sizeof(int *));
+
+  for(i=0;i<c;i++)
+     {
+      x[i]= (int *)calloc(c,sizeof(int));
+      y[i]= (int *)calloc(c,sizeof(int));
+      z[i]= (int *)calloc(c,sizeof(int));
+     }
+
+  return;
+ }
+
+void liberar_matrices(int **x,int **y, int **z, int c)
+{
+  int i;
+  for (i = 0;i < c;i++)
+    {
+     free(x[i]);
+     free(y[i]);
+     free(z[i]);
+    }
  
-  //printf ("This program was called with \"%s\".\n",argv[0]);
-  /*
-  if (argc > 1)
-    {
-      for (count = 1; count < argc; count++)
-    {
-      printf("argv[%d] = %s\n", count, argv[count]);
-    }
-    }
-  else
-    {
-      printf("The command had no other arguments.\n");
-    }
- 
-  return 0;
-  */
-   //printf("Hola %c",*argv[1]);
-   //printf( "Tamaño de la cadena: %i bytes\n", sizeof arqv[1] );
+  free(x);
+  free(y);
+  free(z);
 
-   char *c;
-   int op = strtol(argv[1],&c,10) ;
-   //printf("Hola %lu",op);
-   clock_t start_t, end_t;
-   double duration;
+  return;
+}
 
-    
-    int x=op;
-    //printf("Holaaaaa %lu",x);
-    int k,i,j,y,temporal;
-    srand (time(NULL));
-
-
-
-    int **mat,**mat2, **resultado;
-    //mat = (int **)calloc(x,sizeof(int *));
-    mat = (int **)calloc(x,sizeof(int *));
-    mat2= (int **)calloc(x,sizeof(int *));
-    resultado = (int **)calloc(x,sizeof(int *));
-    
-    for(i=0;i<x;i++)
+void llenar_matriz(int **x,int c)
+{
+    int i,j;
+    for(i=0;i<c;i++)
        {
-        mat[i]= (int *)calloc(x,sizeof(int));
-        mat2[i]= (int *)calloc(x,sizeof(int));
-        resultado[i]= (int *)calloc(x,sizeof(int));
-       }
-	 
-    
-  
-    for(i=0;i<x;i++)
-       {
-        for(j=0;j<x;j++)
-	   {
-	       mat[i][j] = rand() % 11;	    
+        for(j=0;j<c;j++)
+           {
+            x[i][j] = rand() % 11;     
            }
         }
- 
-    
-    for(i=0;i<x;i++)
-       {
-        for(j=0;j<x;j++)
-	    {
-	     mat2[i][j]	= rand() % 11;
-            }
-        }
-            
-            
 
-      start_t = clock();
-   //printf("Starting of the program, start_t = %ld\n", start_t);
-    
-   //printf("Going to scan a big loop, start_t = %ld\n", start_t);
-   
-   
-    for (i = 0 ; i < x ; i++ ) //i para las filas de la matriz resultante
+ return;
+}
+
+int **multiplicar(int **x,int **y,int c)
+{
+  int i,k,j,temporal,** z;
+  for (i = 0 ; i < c ; i++ ) //i para las filas de la matriz resultante
        {
-        for (k = 0 ; k < x ; k++ ) // k para las columnas de la matriz resultante
+        for (k = 0 ; k < c ; k++ ) // k para las columnas de la matriz resultante
            {  
             temporal = 0 ;
-            for (j = 0 ; j < x ; j++ ) //j para realizar la multiplicacion de 
+            for (j = 0 ; j < c ; j++ ) //j para realizar la multiplicacion de 
                 {                                   //los elementos   de la matriz
-                 temporal += mat[i][j] * mat2[j][k];
-                 resultado[i][k] = temporal ;
+                 temporal += x[i][j] * y[j][k];
+                 z[i][k] = temporal ;
                 }
             }
-    	}
-      
-    	end_t = clock();
-      duration = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-      printf("%f\n",duration);
+        }
+ return z;
+}
 
-  /*
-  printf("matriz A:");
-
-  for(i=0;i<x;i++)
-     {
-      printf("\n\t\t");
-      for(j=0;j<x;j++)
-         {
-    printf("[ %d ]", mat[i][j]);
-         }
-     }
-     
-  printf("\nmatriz B:");
-  for(i=0;i<x;i++)
-     {
-      printf("\n\t\t");
-      for(j=0;j<x;j++)
-         {
-        printf("[ %d ]", mat2[i][j]);
-         }
-     }
-
-
-    printf("\n");
-    printf("LA MATRIZ PRODUCTO DE LAS 2 MATRICES INDICADAS ES:\n");
-    for(i=0;i<x;i++)
+void imprimir_matriz(int **x,int c)
+{
+    int i,j;
+    for(i=0;i<c;i++)
        {
         printf("\n\t\t");
-        for(j=0;j<x;j++)
+        for(j=0;j<c;j++)
            {
-  	    printf("[ %d ]", resultado[i][j]);
-	   }
-	}
-  printf("\n");
-
-*/
-
-
-for (i = 0;i < x;i++)
- {
-  free(mat[i]);
-  free(mat2[i]);
-  free(resultado[i]);
- }
- 
-  free(mat);
-  free(mat2);
-  free(resultado);
-
-return 0;
+            printf("[ %d ]", x[i][j]);
+           }
+        }
+     return;
 }
+
+
+
+int main(int argc, char *argv[])
+{
+    srand (time(NULL));
+
+    pid_t pid;
+    clock_t start_t, end_t;
+
+    int k,i,j,y,temporal;
+    int parametro,x;    
+    double duration;
+    int **mat,**mat2, **resultado;    
+
+    parametro = atoi(argv[1]);
+    x = parametro;
+    pid = fork();
+    printf("%d\n", x);
+
+    //reservar_matrices(mat,mat2,resultado,x);
+    //llenar_matriz(mat,x);
+    //llenar_matriz(mat2,x);
+
+    start_t = clock();
+    if (pid == -1) 
+      { 
+        perror ("No se puede crear proceso");
+        exit (-1);
+      }
+    else 
+        if (pid > 0)
+        {
+         reservar_matrices(mat,mat2,resultado,x);
+         llenar_matriz(mat,x);
+         /*llenar_matriz(mat2,x);
+         imprimir_matriz(mat,x);
+         liberar_matrices(mat,mat2,resultado,x);*/
+        }
+         else
+              if (pid == 0)
+              {
+                imprimir_matriz(mat,x);
+                liberar_matrices(mat,mat2,resultado,x);
+              }
+
+    end_t = clock();
+    duration = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("%f\n",duration);
+ 
+    return 0;
+}
+
